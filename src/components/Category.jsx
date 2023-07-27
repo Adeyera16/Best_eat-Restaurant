06d8a8a354c300a568056ad8a7c23598098c0e99
Adeyera <asimolowoelizabeth@gmail.com>
2023-07-27 08:02:01 +0100

    Modified the category file

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,7 +7,7 @@ const Category = () => {
     <div className='max-w-[1640px] px-4 py-12'>
         <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
         {/* categories */}
-        <div className='grid grid-cols-2 md:grid-cols-4 gap-6 py-6'>
+        <div className='grid grid-cols-2 md:grid-cols-4 m-auto gap-6 py-6'>
         {
             categories.map((item, index) =>(
                 <div key={index} className='bg-gray-100 rounded-lg p-4 flex justify-between items-center'>
